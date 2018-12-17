4a915cbd52a9a8c088ca6636a39509a2aad10f33
ifa82jr <j-rutsch@web.de>
2018-12-18 00:55:09 +0100

    Added function is_leapyear.

--- main.c
+++ main.c
@@ -8,6 +8,18 @@
 #include <stdlib.h>
 
 /**
+ * Proves if the given year is an leap-year and if the year is not before 1582.
+ **/
+int is_leapyear(int year)
+{
+    if (year < 1582) {
+        return -1;
+    }
+
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+/**
  *
  *
  **/
@@ -20,5 +32,9 @@ int day_of_the_year(int day, int month, int year)
 int main()
 {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+    printf("Jahr 1999 ist: %i\n", is_leapyear(1999));
+    printf("Jahr 2000 ist: %i\n", is_leapyear(2000));
+    printf("Jahr 1980 ist: %i\n", is_leapyear(1980));
+    printf("Jahr 1980 ist: %i\n", is_leapyear(1400));
     return 0;
 }
