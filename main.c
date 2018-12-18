f5d20e729dd229d032ed87a929edb5db32bdc307
ifa82jr <j-rutsch@web.de>
2018-12-18 01:15:31 +0100

    Added function get_days_for_month

--- main.c
+++ main.c
@@ -8,7 +8,28 @@
 #include <stdlib.h>
 
 /**
- * Proves if the given year is an leap-year and if the year is not before 1582.
+ *  This function determines the days of a given month in the given year.
+ *  The given month needs to be between 1 and 12. Leap-years will be considered.
+ **/
+int get_days_for_month(int month, int year)
+{
+    if (month < 1 || month > 12 || year < 1582) {
+        return -1;
+    }
+
+    int days[13] = {
+        0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    };
+
+    if (is_leapyear(year)) {
+        days[2] = 29;
+    }
+
+    return days[month];
+}
+
+/**
+ *  Proves if the given year is an leap-year and if the year is not before 1582.
  **/
 int is_leapyear(int year)
 {
@@ -35,6 +56,9 @@ int main()
     printf("Jahr 1999 ist: %i\n", is_leapyear(1999));
     printf("Jahr 2000 ist: %i\n", is_leapyear(2000));
     printf("Jahr 1980 ist: %i\n", is_leapyear(1980));
-    printf("Jahr 1980 ist: %i\n", is_leapyear(1400));
+    printf("Jahr 1400 ist: %i\n", is_leapyear(1400));
+    printf("Januar: %i\n", get_days_for_month(1, 1800));
+    printf("Februar: %i\n", get_days_for_month(2, 1980));
+    printf("Februar: %i\n", get_days_for_month(2, 1979));
     return 0;
 }
